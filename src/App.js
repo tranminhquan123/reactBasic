b37cc1aede635c47135560ac03a07c2871e9daef
tranminhquan123 <157604927+tranminhquan123@users.noreply.github.com>
2024-12-17 20:11:33 +0700

    my mess

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          tran minh quan hoc vien hang khong
         </p>
         <a
           className="App-link"
